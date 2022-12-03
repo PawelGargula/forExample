4089d9e589ed61f8e430d24d7e877959fd8f6121
Paweł Gargula <pawel.gargula@interia.pl>
2022-12-03 13:05:59 +0100

    Fix creating periods. Before, it worked only for current period of "2014-01".

--- create-periods.js
+++ create-periods.js
@@ -1,17 +1,25 @@
-const currentPeriod = new Date("2014-01");
+const currentPeriod = new Date("2014-6");
+const currentPeriodString = `${currentPeriod.getFullYear()}-${(currentPeriod.getMonth() + 1).toString().padStart(2, "0")}`;
 
+const currentPeriodDOM = document.querySelector("#current-period");
 const periodsNumberDOM = document.querySelector("input");
 const createdPeriodsDOM = document.querySelector("#created-periods");
 
+currentPeriodDOM.textContent += currentPeriodString;
+createdPeriodsDOM.textContent += currentPeriodString;
+
 periodsNumberDOM.addEventListener("input", () => {
-    let periodsToCreate = Number(periodsNumberDOM.value) < 1 || Number(periodsNumberDOM.value) > 10_000
+    const value = Math.floor(Number(periodsNumberDOM.value));
+    const periodsToCreate = value < 1 || value > 10_000
         ? 1
-        : Math.floor(Number(periodsNumberDOM.value));
-    let periodsString = new Array(periodsToCreate)
+        : value;
+    const periodsString = new Array(periodsToCreate)
         .fill(null)
         .map((val, index) => {
-            let year = currentPeriod.getFullYear() + Math.floor(index / 12);
-            let month = currentPeriod.getMonth() + 1 + index % 12;
+            const year = currentPeriod.getFullYear() + Math.floor((currentPeriod.getMonth() + index) / 12);
+            let month = (currentPeriod.getMonth() + 1 + index) % 12;
+            if (month === 0)
+                month = 12;
             return `${year}-${month.toString().padStart(2, "0")}`;
         })
         .join(" / ");
